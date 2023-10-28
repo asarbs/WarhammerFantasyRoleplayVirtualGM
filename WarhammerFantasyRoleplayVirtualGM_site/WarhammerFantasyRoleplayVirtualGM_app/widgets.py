e1e8d29c8d91176b913d9e2cb10ff66daa7e97ea
asar <asar.bs@gmail.com>
2023-10-28 17:13:29 +0200

    fix bug for none wealth in  PriceCharFiled

--- WarhammerFantasyRoleplayVirtualGM_site/WarhammerFantasyRoleplayVirtualGM_app/widgets.py
+++ WarhammerFantasyRoleplayVirtualGM_site/WarhammerFantasyRoleplayVirtualGM_app/widgets.py
@@ -12,6 +12,8 @@ class PriceCharFiled(CharField):
         super().__init__(*args, **kwargs)
 
     def prepare_value(self, wealth):
+        if wealth is None:
+            return "{} GC {}/{}".format(0, 0, 0)
         GC = math.floor(wealth / 240)
         GC_left = wealth % 240
         SC = math.floor(GC_left / 12)
