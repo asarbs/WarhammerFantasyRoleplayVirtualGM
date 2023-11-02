2b7f96383fd3c722cfaad3517a91cae2ab2932b3
asar <asar.bs@gmail.com>
2023-11-02 18:55:41 +0100

    backup script

--- tools/backup.sh
+++ tools/backup.sh
@@ -2,8 +2,14 @@
 
 set -o xtrace
 
+
+HOSTNAME=$(hostname)
 PWD=/workspaces/WarhammerFantasyRoleplayVirtualGM/WarhammerFantasyRoleplayVirtualGM_site
+if [ "$HOSTNAME" = warhammer ]; then
+    PWD=/var/www/django/WarhammerFantasyRoleplayVirtualGM/WarhammerFantasyRoleplayVirtualGM_site
+    source /var/www/django/WarhammerFantasyRoleplayVirtualGM/wfrpg_venv/bin/activate
+fi
 TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
 
 find  ${PWD}/backup -type f -ctime +7 | xargs rm -rf
-/workspaces/WarhammerFantasyRoleplayVirtualGM/WarhammerFantasyRoleplayVirtualGM_site/manage.py dumpdata > ${PWD}/backup/${TIMESTAMP}_wfrpg.json
+${PWD}/manage.py dumpdata > ${PWD}/backup/${TIMESTAMP}_wfrpg.json
