3f5768d7f91bafdf865904987baf54794e3ab65d
Bartosz Skorupa <asar.bs@gmail.com>
2025-05-20 20:44:59 +0200

    add signal receive when add new user via google

--- WarhammerFantasyRoleplayVirtualGM_site/WarhammerFantasyRoleplayVirtualGM_app/apps.py
+++ WarhammerFantasyRoleplayVirtualGM_site/WarhammerFantasyRoleplayVirtualGM_app/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class WarhammerfantasyroleplayvirtualgmAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'WarhammerFantasyRoleplayVirtualGM_app'
+
+    def ready(self):
+        import WarhammerFantasyRoleplayVirtualGM_app.signals
